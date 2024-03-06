9ad1f2ff076921db6aa922b01c3afb0d4004c1f6
MO3GEZA619 <moody619.ma@gmail.com>
2024-03-06 03:55:39 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createClient } from '@supabase/supabase-js';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
+const supabase = createClient(
+  "https://dfbirycaxkroaavmdfbp.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRmYmlyeWNheGtyb2Fhdm1kZmJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk2NTk4ODQsImV4cCI6MjAyNTIzNTg4NH0.KqlO4S2xlVx4jno18sUknosEBj40Nzmc0ycO7ZRbKSw" 
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SessionContextProvider supabaseClient={supabase}>
+      <App />
+    </SessionContextProvider>
   </React.StrictMode>
 );
 
